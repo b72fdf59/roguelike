c8a41f77ac47977c0b9ee7d2b5a657154664601e
Adwithya Magow <avmagow@gmail.com>
2024-01-09 14:27:21 +0800

    feat(roguelike): 2.2 walking a map

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,9 @@ impl GameState for State {
         player_input(self, ctx);
         self.run_systems();
 
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
@@ -68,10 +71,14 @@ struct Player {}
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let players = ecs.read_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     for (_player, pos) in (&players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let destination_idx = xs_yx_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
@@ -88,6 +95,83 @@ fn player_input(gs: &mut State, ctx: &mut Rltk) {
     }
 }
 
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall,
+    Floor,
+}
+
+fn xs_yx_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80 * 50];
+
+    for x in 0..80 {
+        map[xs_yx_idx(x, 0)] = TileType::Wall;
+        map[xs_yx_idx(x, 49)] = TileType::Wall;
+    }
+
+    for y in 0..50 {
+        map[xs_yx_idx(0, y)] = TileType::Wall;
+        map[xs_yx_idx(79, y)] = TileType::Wall;
+    }
+
+    // Now we'll randomly splat a bunch of walls. It won't be pretty, but it's a decent illustration.
+    // First, obtain the thread-local RNG:
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    // Next we choose how many walls to make
+    let num_walls = 400;
+    for _i in 0..num_walls {
+        // For each wall we want to randomly choose a position that is inside the arena
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+
+        // We need to convert our X/Y coordinates into an array index
+        let idx = xs_yx_idx(x, y);
+
+        // And finally, we change the tile at the index to a wall
+        map[idx] = TileType::Wall;
+    }
+
+    map
+}
+
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut x = 0;
+    let mut y = 0;
+    for tile in map.iter() {
+        match tile {
+            TileType::Floor => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.5, 0.5, 0.5),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('.'),
+                );
+            }
+            TileType::Wall => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0., 1.0, 0.),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('#'),
+                );
+            }
+        }
+
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
 fn main() {
     let mut gs = State { ecs: World::new() };
     gs.ecs.register::<Position>();
@@ -100,18 +184,7 @@ fn main() {
         .build()
         .unwrap();
 
-    for i in 0..10 {
-        gs.ecs
-            .create_entity()
-            .with(Position { x: i * 7, y: 20 })
-            .with(Renderable {
-                glyph: rltk::to_cp437('☺'),
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .with(LeftMover {})
-            .build();
-    }
+    gs.ecs.insert(new_map());
 
     gs.ecs
         .create_entity()
